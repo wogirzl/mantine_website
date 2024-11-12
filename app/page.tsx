cc59c3c089f1c2104ab6bd60a6a2b54069c6b996
wogirzl <l.wogirz@gmail.com>
2024-11-12 16:33:45 +0100

    first experiments

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { Welcome } from '../components/Welcome/Welcome';
+import { Header } from '../components/Header/Header';
+import { Footer } from '../components/Footer/Footer';
+import { HeroTitle } from '../components/HeroTitle/HeroTitle';
 
 export default function HomePage() {
   return (
     <>
+      <Header />
+      <HeroTitle />
       <Welcome />
       <ColorSchemeToggle />
+      <Footer />
     </>
   );
 }
