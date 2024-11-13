a9c2a592ec4848920d5d13eb8feab0d0846121bd
wogirzl <l.wogirz@gmail.com>
2024-11-13 09:03:49 +0100

    added carousel

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,14 @@ import { Welcome } from '../components/Welcome/Welcome';
 import { Header } from '../components/Header/Header';
 import { Footer } from '../components/Footer/Footer';
 import { HeroTitle } from '../components/HeroTitle/HeroTitle';
+import { CardsCarousel } from '../components/CardsCarousel/CardsCarousel';
 
 export default function HomePage() {
   return (
     <>
       <Header />
       <HeroTitle />
+      <CardsCarousel />
       <Welcome />
       <ColorSchemeToggle />
       <Footer />
